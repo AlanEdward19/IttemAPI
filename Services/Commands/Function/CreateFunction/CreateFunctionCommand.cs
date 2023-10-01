46c3b7749c5c0ada41879f28f168119303c1d185
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-10-01 16:32:06 -0300

    feat: CreateFunction command

---
+++ Services/Commands/Function/CreateFunction/CreateFunctionCommand.cs
@@ -0,0 +1,15 @@
+﻿namespace Services.Commands.Function.CreateFunction;
+
+public class CreateFunctionCommand
+{
+    public string Name { get; set; }
+
+    public Domain.Entities.Function ToEntity()
+    {
+        return new()
+        {
+            Id = Guid.NewGuid(),
+            Name = this.Name
+        };
+    }
+}
---
+++ Services/Commands/Function/CreateFunction/CreateFunctionCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using Infrastructure.Context;
+
+namespace Services.Commands.Function.CreateFunction;
+
+public class CreateFunctionCommandHandler
+{
+    private readonly IttemContext _dbContext;
+
+    public CreateFunctionCommandHandler(IttemContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<dynamic> CreateInstructor(CreateFunctionCommand command)
+    {
+        var parsedEntity = command.ToEntity();
+        await _dbContext.Functions.AddAsync(parsedEntity);
+
+        await _dbContext.SaveChangesAsync();
+
+        return new
+        {
+            parsedEntity.Id,
+            Function = parsedEntity.Name
+        };
+    }
+}
