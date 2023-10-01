cd8b6af453afb12ce0aaa48145d2d4a7f87466a0
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-10-01 16:32:24 -0300

    feat: CreateStudent command

---
+++ Services/Commands/Student/CreateStudent/CreateStudentCommand.cs
@@ -0,0 +1,57 @@
+﻿using Domain.Enums;
+
+namespace Services.Commands.Student.CreateStudent;
+public class CreateStudentCommand
+{
+    public string Cpf { get; set; }
+    public string Name { get; set; }
+    public string City { get; set; }
+    public DateTime BirthDate { get; set; }
+    public string CompanyCnpj { get; set; }
+    public Guid FunctionId { get; set; }
+    public Guid ClassId { get; set; }
+    public int PracticeHours { get; set; }
+    public int TheoreticalHours { get; set; }
+    public EModel Model { get; set; }
+    public EReasonForTermination ReasonForTermination { get; set; }
+    public int ContractPeriod { get; set; }
+    public bool Status { get; set; } //Ativo ou Inativo
+    public string PhoneNumber { get; set; }
+    public Guid PoloId { get; set; }
+    public DateTime EndDate { get; set; }
+    public DateTime AdmissionDate { get; set; }
+    public int FirstDayOfTrainingIntroduction { get; set; }
+    public int FinalDayTrainingIntroduction { get; set; }
+    public int FirstDayOfWeeklyTraining { get; set; }
+    public EWeekDay DayOfTrainingWeek { get; set; }
+    public string ScheduleTrainingInitialEFinal { get; set; }
+
+    public Domain.Entities.Student ToEntity()
+    {
+        return new()
+        {
+            Cpf = Cpf,
+            Name = Name,
+            City = City,
+            BirthDate = DateTime.SpecifyKind(BirthDate, DateTimeKind.Utc),
+            CompanyId = CompanyCnpj,
+            FunctionId = FunctionId,
+            ClassId = ClassId,
+            PracticeHours = PracticeHours,
+            TheoreticalHours = TheoreticalHours,
+            Model = Model,
+            ReasonForTermination = ReasonForTermination,
+            ContractPeriod = ContractPeriod,
+            Status = Status,
+            PhoneNumber = PhoneNumber,
+            PoloId = PoloId,
+            EndDate = DateTime.SpecifyKind(EndDate, DateTimeKind.Utc),
+            AdmissionDate = DateTime.SpecifyKind(AdmissionDate, DateTimeKind.Utc),
+            FirstDayOfTrainingIntroduction = FirstDayOfTrainingIntroduction,
+            FinalDayTrainingIntroduction = FinalDayTrainingIntroduction,
+            FirstDayOfWeeklyTraining = FirstDayOfWeeklyTraining,
+            DayOfTrainingWeek = DayOfTrainingWeek,
+            ScheduleTrainingInitialEFinal = ScheduleTrainingInitialEFinal
+        };
+    }
+}
---
+++ Services/Commands/Student/CreateStudent/CreateStudentCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using Infrastructure.Context;
+
+namespace Services.Commands.Student.CreateStudent;
+
+public class CreateStudentCommandHandler
+{
+    private readonly IttemContext _dbContext;
+
+    public CreateStudentCommandHandler(IttemContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<dynamic> CreateStudent(CreateStudentCommand command)
+    {
+        var parsedEntity = command.ToEntity();
+        await _dbContext.Students.AddAsync(parsedEntity);
+
+        await _dbContext.SaveChangesAsync();
+
+        return new
+        {
+            parsedEntity.Cpf,
+            Student = parsedEntity.Name
+        };
+    }
+}
