b3a78e53e99ae71b358da6986e764b11d7ddaf33
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-10-01 16:31:57 -0300

    feat: CreateCompany command

---
+++ Services/Commands/Company/CreateCompany/CreateCompanyCommand.cs
@@ -0,0 +1,16 @@
+﻿namespace Services.Commands.Company.CreateCompany;
+
+public class CreateCompanyCommand
+{
+    public string Cnpj { get; set; }
+    public string Name { get; set; }
+
+    public Domain.Entities.Company ToEntity()
+    {
+        return new()
+        {
+            Name = this.Name,
+            Cnpj = this.Cnpj
+        };
+    }
+}
---
+++ Services/Commands/Company/CreateCompany/CreateCompanyCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using Infrastructure.Context;
+
+namespace Services.Commands.Company.CreateCompany;
+
+public class CreateCompanyCommandHandler
+{
+    private readonly IttemContext _dbContext;
+
+    public CreateCompanyCommandHandler(IttemContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<dynamic> CreateCompany(CreateCompanyCommand command)
+    {
+        var parsedEntity = command.ToEntity();
+        await _dbContext.Companies.AddAsync(parsedEntity);
+
+        await _dbContext.SaveChangesAsync();
+
+        return new
+        {
+            parsedEntity.Cnpj,
+            Company = parsedEntity.Name
+        };
+    }
+}
