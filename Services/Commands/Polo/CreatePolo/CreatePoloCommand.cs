66eb28ea3189dc90f7ac67cef859f01700adb498
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-10-01 16:32:30 -0300

    feat: CreatePolo command

---
+++ Services/Commands/Polo/CreatePolo/CreatePoloCommand.cs
@@ -0,0 +1,15 @@
+﻿namespace Services.Commands.Polo.CreatePolo;
+
+public class CreatePoloCommand
+{
+    public string Name { get; set; }
+
+    public Domain.Entities.Polo ToEntity()
+    {
+        return new()
+        {
+            Id = Guid.NewGuid(),
+            Name = this.Name
+        };
+    }
+}
---
+++ Services/Commands/Polo/CreatePolo/CreatePoloCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using Infrastructure.Context;
+
+namespace Services.Commands.Polo.CreatePolo;
+
+public class CreatePoloCommandHandler
+{
+    private readonly IttemContext _dbContext;
+
+    public CreatePoloCommandHandler(IttemContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<dynamic> CreatePolo(CreatePoloCommand command)
+    {
+        var parsedEntity = command.ToEntity();
+        await _dbContext.Polos.AddAsync(parsedEntity);
+
+        await _dbContext.SaveChangesAsync();
+
+        return new
+        {
+            parsedEntity.Id,
+            Polo = parsedEntity.Name
+        };
+    }
+}
