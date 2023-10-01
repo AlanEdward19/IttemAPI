92a9cecd541d45f21252a8644aac7479c37cca3e
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-10-01 16:31:47 -0300

    feat: CreateClass command

---
+++ Services/Commands/Class/CreateClass/CreateClassCommand.cs
@@ -0,0 +1,17 @@
+﻿namespace Services.Commands.Class.CreateClass;
+
+public class CreateClassCommand
+{
+    public string Name { get; set; }
+    public Guid InstructorId { get; set; }
+
+    public Domain.Entities.Class ToEntity()
+    {
+        return new()
+        {
+            Id = Guid.NewGuid(),
+            Name = this.Name,
+            InstructorId = InstructorId
+        };
+    }
+}
---
+++ Services/Commands/Class/CreateClass/CreateClassCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using Infrastructure.Context;
+
+namespace Services.Commands.Class.CreateClass;
+
+public class CreateClassCommandHandler
+{
+    private readonly IttemContext _dbContext;
+
+    public CreateClassCommandHandler(IttemContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<dynamic> CreateClass(CreateClassCommand command)
+    {
+        var parsedEntity = command.ToEntity();
+        await _dbContext.Classes.AddAsync(parsedEntity);
+
+        await _dbContext.SaveChangesAsync();
+
+        return new
+        {
+            parsedEntity.Id,
+            Class = parsedEntity.Name
+        };
+    }
+}
