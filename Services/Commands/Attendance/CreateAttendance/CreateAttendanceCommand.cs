b6bfd6a6e4da175ccdb13e6c4c0ffe86e30a0027
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-10-01 16:31:38 -0300

    feat: CreateAttendance command

---
+++ Services/Commands/Attendance/CreateAttendance/CreateAttendanceCommand.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Enums;
+
+namespace Services.Commands.Attendance.CreateAttendance;
+
+public class CreateAttendanceCommand
+{
+    public string StudentCpf { get; set; }
+    public string Level { get; set; }
+    public string Module { get; set; }
+    public EAttendance Type { get; set; }
+
+    public Domain.Entities.Attendance ToEntity()
+    {
+        return new()
+        {
+            Id = Guid.NewGuid(),
+            StudentId = this.StudentCpf,
+            Level = this.Level,
+            Module = this.Module,
+            Type = this.Type,
+            Date = DateTime.SpecifyKind(DateTime.Today, DateTimeKind.Utc)
+        };
+    }
+}
---
+++ Services/Commands/Attendance/CreateAttendance/CreateAttendanceCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using Infrastructure.Context;
+
+namespace Services.Commands.Attendance.CreateAttendance;
+
+public class CreateAttendanceCommandHandler
+{
+    private readonly IttemContext _dbContext;
+
+    public CreateAttendanceCommandHandler(IttemContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<dynamic> CreateAttendance(CreateAttendanceCommand command)
+    {
+        var parsedEntity = command.ToEntity();
+        await _dbContext.Attendances.AddAsync(parsedEntity);
+
+        await _dbContext.SaveChangesAsync();
+
+        return new
+        {
+            parsedEntity.Id,
+            Type = parsedEntity.Type
+        };
+    }
+}
