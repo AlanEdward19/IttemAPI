d311821cb62b609a791941b9cb249eda1c89f24e
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-10-01 16:31:28 -0300

    feat: CreateAssessment command

---
+++ Services/Commands/Assessment/CreateAssessment/CreateAssessmentCommand.cs
@@ -0,0 +1,27 @@
+﻿namespace Services.Commands.Assessment.CreateAssessment;
+
+public class CreateAssessmentCommand
+{
+    public string StudentCpf { get; set; }
+    public string Level { get; set; }
+    public string Module { get; set; }
+    public int SkillTechnique { get; set; }
+    public int Participation { get; set; }
+    public int InterpersonalRelationship { get; set; }
+    public int GoalFulfillment { get; set; }
+
+    public Domain.Entities.Assessment ToEntity()
+    {
+        return new()
+        {
+            Id = Guid.NewGuid(),
+            Level = this.Level,
+            Module = this.Module,
+            SkillTechnique = this.SkillTechnique,
+            Participation = this.Participation,
+            InterpersonalRelationship = this.InterpersonalRelationship,
+            GoalFulfillment = this.GoalFulfillment,
+            StudentId = StudentCpf
+        };
+    }
+}
---
+++ Services/Commands/Assessment/CreateAssessment/CreateAssessmentCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using Infrastructure.Context;
+
+namespace Services.Commands.Assessment.CreateAssessment;
+
+public class CreateAssessmentCommandHandler
+{
+    private readonly IttemContext _dbContext;
+
+    public CreateAssessmentCommandHandler(IttemContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<dynamic> CreateAssessment(CreateAssessmentCommand command)
+    {
+        var parsedEntity = command.ToEntity();
+        await _dbContext.Assessments.AddAsync(parsedEntity);
+
+        await _dbContext.SaveChangesAsync();
+
+        return new
+        {
+            parsedEntity.Id
+        };
+    }
+}
