433b0594df7ff63a6c27cebea4b6898d628dc23f
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-10-01 16:32:16 -0300

    feat: CreateInstructor command

---
+++ Services/Commands/Instructor/CreateInstructor/CreateInstructorCommand.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Entities;
+using Infrastructure.Context;
+
+namespace Services.Commands.Instructor.CreateInstructor;
+
+public class CreateInstructorCommand
+{
+    public string Name { get; set; }
+    public string PhotoPath { get; set; }
+    public List<Guid>? ClassesId { get; set; }
+
+
+    public Domain.Entities.Instructor ToEntity(IttemContext context)
+    {
+        var classes = ClassesId != null && ClassesId.Any()
+            ? context.Classes.Where(x => ClassesId.Contains(x.Id)).ToList()
+            : null;
+
+        return new()
+        {
+            Id = Guid.NewGuid(),
+            Name = this.Name,
+            PhotoPath = this.PhotoPath,
+            Classes = classes
+        };
+    }
+}
---
+++ Services/Commands/Instructor/CreateInstructor/CreateInstructorCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using Infrastructure.Context;
+
+namespace Services.Commands.Instructor.CreateInstructor;
+
+public class CreateInstructorCommandHandler
+{
+    private readonly IttemContext _dbContext;
+
+    public CreateInstructorCommandHandler(IttemContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<dynamic> CreateInstructor(CreateInstructorCommand command)
+    {
+        var parsedEntity = command.ToEntity(_dbContext);
+        await _dbContext.Instructors.AddAsync(parsedEntity);
+
+        await _dbContext.SaveChangesAsync();
+
+        return new
+        {
+            parsedEntity.Id,
+            Instructor = parsedEntity.Name
+        };
+    }
+}
