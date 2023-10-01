fee9684d3da7381e9ef0f40f5b41dffd67957957
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-10-01 16:32:43 -0300

    feat: CreateCertificate command, but without handler

---
+++ Services/Commands/CertificateIssuance/CreateCertificate/CreateCertificateCommand.cs
@@ -0,0 +1,8 @@
+﻿namespace Services.Commands.CertificateIssuance.CreateCertificate;
+
+public class CreateCertificateCommand
+{
+    public Guid Id { get; set; }
+    public Guid StudentId { get; set; }
+    public DateTime DateOfIssue { get; set; }
+}
