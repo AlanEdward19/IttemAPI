afb575c45e3ce85a57989b056e076b25f3aef439
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-10-02 14:10:54 -0300

    feat: getStudentByClass implemented

--- Services/Queries/Student/GetStudent/GetStudentQueryHandler.cs
+++ Services/Queries/Student/GetStudent/GetStudentQueryHandler.cs
@@ -59,4 +59,50 @@ public class GetStudentQueryHandler
 
         return result;
     }
+
+    public async Task<IEnumerable<StudentViewModel>> GetStudentByClass(string? classId)
+    {
+        List<StudentViewModel> result = new();
+        var database = string.IsNullOrWhiteSpace(classId)
+            ? await _dbContext.Students.Include(x => x.Company).Include(x => x.Class).Include(x => x.Function)
+                .Include(x => x.Polo).ToListAsync()
+            : await _dbContext.Students.Include(x => x.Company).Include(x => x.Class).Include(x => x.Function)
+                .Include(x => x.Polo).Where(x => x.ClassId.Equals(Guid.Parse(classId)))
+                .ToListAsync();
+
+        foreach (var student in database)
+        {
+            result.Add(new()
+            {
+                Cpf = student.Cpf,
+                AdmissionDate = student.AdmissionDate,
+                BirthDate = student.BirthDate,
+                City = student.City,
+                ClassId = student.ClassId,
+                ClassName = student.Class.Name,
+                CompanyId = student.CompanyId,
+                CompanyName = student.Company.Name,
+                ContractPeriod = student.ContractPeriod,
+                DayOfTrainingWeek = student.DayOfTrainingWeek.ToString(),
+                EndDate = student.EndDate,
+                FinalDayTrainingIntroduction = student.FinalDayTrainingIntroduction,
+                FirstDayOfTrainingIntroduction = student.FirstDayOfTrainingIntroduction,
+                FirstDayOfWeeklyTraining = student.FirstDayOfWeeklyTraining,
+                FunctionId = student.FunctionId,
+                FunctionName = student.Function.Name,
+                Model = student.Model.ToString(),
+                Name = student.Name,
+                ReasonForTermination = student.ReasonForTermination.ToString(),
+                PhoneNumber = student.PhoneNumber,
+                PoloId = student.PoloId,
+                PoloName = student.Polo.Name,
+                PracticeHours = student.PracticeHours,
+                ScheduleTrainingInitialEFinal = student.ScheduleTrainingInitialEFinal,
+                Status = student.Status ? "Ativo" : "Inativo",
+                TheoreticalHours = student.TheoreticalHours
+            });
+        }
+
+        return result;
+    }
 }
