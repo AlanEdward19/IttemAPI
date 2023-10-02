5d5c81a8fba3af7b5c9442880663ea3865ce18ea
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-10-02 14:09:39 -0300

    feat: delete command for attendance

---
+++ Services/Commands/Assessment/DeleteAssessment/DeleteAssessmentCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Entities;
+using Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Services.Commands.Assessment.DeleteAssessment;
+
+public class DeleteAssessmentCommandHandler
+{
+    private readonly IttemContext _dbContext;
+
+    public DeleteAssessmentCommandHandler(IttemContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<dynamic> DeleteAssessment(string? id)
+    {
+        var assessment = await _dbContext.Assessments.FirstAsync(x => x.Id.Equals(Guid.Parse(id)));
+        _dbContext.Assessments.Remove(assessment);
+
+        await _dbContext.SaveChangesAsync();
+
+        return new
+        {
+            Operation = "Delete",
+            AssessmentId = assessment.Id,
+            StudentId = assessment.StudentId
+        };
+    }
+}
