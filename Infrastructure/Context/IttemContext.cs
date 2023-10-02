9a9ef0fc7bf050c516b920a203084cc4ad28da46
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-10-02 14:09:07 -0300

    feat: cascade delete behavior for student

--- Infrastructure/Context/IttemContext.cs
+++ Infrastructure/Context/IttemContext.cs
@@ -8,6 +8,50 @@ public class IttemContext : DbContext
     public IttemContext(DbContextOptions<IttemContext> options) : base(options)
     { }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Student>()
+            .HasMany(s => s.Attendances)
+            .WithOne(a => a.Student)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<Student>()
+            .HasMany(s => s.Assessments)
+            .WithOne(a => a.Student)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<Student>()
+            .HasMany(s => s.CertificateIssuances)
+            .WithOne(ci => ci.Student)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<Class>()
+            .HasMany(c => c.Students)
+            .WithOne(s => s.Class)
+            .OnDelete(DeleteBehavior.SetNull);
+
+        modelBuilder.Entity<Company>()
+            .HasMany(c => c.Students)
+            .WithOne(s => s.Company)
+            .OnDelete(DeleteBehavior.SetNull);
+
+        modelBuilder.Entity<Function>()
+            .HasMany(f => f.Students)
+            .WithOne(s => s.Function)
+            .OnDelete(DeleteBehavior.SetNull);
+
+        modelBuilder.Entity<Student>()
+            .HasOne(s => s.Polo)
+            .WithMany(p => p.Students)
+            .HasForeignKey(s => s.PoloId)
+            .OnDelete(DeleteBehavior.SetNull);
+
+        // Adicione outras configurações do modelo conforme necessário...
+
+        base.OnModelCreating(modelBuilder);
+    }
+
+
     #region DbSets
 
     public DbSet<Assessment> Assessments { get; set; }
