6df3346c1fe5e19d107bdde495ef692867a263d2
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-10-02 14:10:23 -0300

    feat: delete command for student

---
+++ Services/Commands/Student/DeleteStudent/DeleteStudentCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Services.Commands.Student.DeleteStudent;
+
+public class DeleteStudentCommandHandler
+{
+    private readonly IttemContext _dbContext;
+
+    public DeleteStudentCommandHandler(IttemContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<dynamic> Delete(string id)
+    {
+        var student = await _dbContext.Students.FirstAsync(x => x.Cpf.Equals(id));
+
+        _dbContext.Students.Remove(student);
+
+        await _dbContext.SaveChangesAsync();
+
+        return new
+        {
+            Operation = "Delete",
+            StudentId = student.Cpf
+        };
+    }
+}
