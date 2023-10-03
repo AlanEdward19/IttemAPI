ebb0634d9ebe6e681a29c5cf426e43f3ce000afe
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-10-03 09:58:02 -0300

    feat: new viewModels

--- Services/ViewModels/InstructorViewModel.cs
+++ Services/ViewModels/InstructorViewModel.cs
@@ -3,6 +3,7 @@
 public class InstructorViewModel
 {
     public Guid Id { get; set; }
-    public string Name { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
     public string PhotoPath { get; set; }
 }
---
+++ Services/ViewModels/LoginViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Services.ViewModels;
+
+public class LoginViewModel
+{
+    public string Email { get; set; }
+    public string Token { get; set; }
+}
