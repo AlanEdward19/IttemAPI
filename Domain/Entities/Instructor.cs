3fe8fd3e72982b41565b8a1d79bbcbe85f597aed
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-10-03 09:55:04 -0300

    feat: added user data for login on instructors

--- Domain/Entities/Instructor.cs
+++ Domain/Entities/Instructor.cs
@@ -4,8 +4,12 @@ public class Instructor
 {
     [Key]
     public Guid Id { get; set; }
-    public string Name { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
     public string PhotoPath { get; set; }
+    public string Role { get; set; }
 
     public virtual IEnumerable<Class>? Classes { get; set; }
 }
