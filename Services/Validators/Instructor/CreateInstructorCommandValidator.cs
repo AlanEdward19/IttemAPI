66dd5e9d29799f283d01c1550fbbb80285f036ae
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-10-03 09:57:50 -0300

    feat: instructor info validator

---
+++ Services/Validators/Instructor/CreateInstructorCommandValidator.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation;
+using Services.Commands.Instructor.CreateInstructor;
+using System.Text.RegularExpressions;
+
+namespace Services.Validators.Instructor;
+
+public class CreateInstructorCommandValidator : AbstractValidator<CreateInstructorCommand>
+{
+    public CreateInstructorCommandValidator()
+    {
+        RuleFor(p => p.Email)
+            .EmailAddress()
+            .WithMessage("E-mail não válido!");
+
+        RuleFor(p => p.Password)
+            .Must(ValidPassword)
+            .WithMessage("Senha deve conter pelo menos 8 caracteres, um número, uma letra maiúscula, uma minúscula, e um caractere especial");
+
+        RuleFor(p => p.FirstName)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("Primeiro nome é obrigatório!");
+
+        RuleFor(p => p.LastName)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("Ultimo nome é obrigatório!");
+    }
+
+    public bool ValidPassword(string password)
+    {
+        var regex = new Regex(@"^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!*@#$%^&+=]).*$");
+
+        return regex.IsMatch(password);
+    }
+}
