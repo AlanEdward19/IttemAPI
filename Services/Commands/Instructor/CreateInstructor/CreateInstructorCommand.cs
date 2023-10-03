fc00e419444bc48a8db7c162c7dce21ca8793998
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-10-03 09:56:44 -0300

    feat: updated instructor command create

--- Services/Commands/Instructor/CreateInstructor/CreateInstructorCommand.cs
+++ Services/Commands/Instructor/CreateInstructor/CreateInstructorCommand.cs
@@ -1,16 +1,22 @@
 ﻿using Domain.Entities;
+using Domain.Enums;
+using Domain.Interfaces;
 using Infrastructure.Context;
+using Services.Auth;
 
 namespace Services.Commands.Instructor.CreateInstructor;
 
 public class CreateInstructorCommand
 {
-    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
     public string PhotoPath { get; set; }
+    public ERole Role { get; set; }
     public List<Guid>? ClassesId { get; set; }
 
-
-    public Domain.Entities.Instructor ToEntity(IttemContext context)
+    public Domain.Entities.Instructor ToEntity(IttemContext context, IAuthService authService)
     {
         var classes = ClassesId != null && ClassesId.Any()
             ? context.Classes.Where(x => ClassesId.Contains(x.Id)).ToList()
@@ -19,7 +25,11 @@ public class CreateInstructorCommand
         return new()
         {
             Id = Guid.NewGuid(),
-            Name = this.Name,
+            Email = this.Email,
+            Password = authService.ComputeSha256Hash(this.Password),
+            FirstName = this.FirstName,
+            LastName = this.LastName,
+            Role = this.Role.ToString(),
             PhotoPath = this.PhotoPath,
             Classes = classes
         };
--- Services/Commands/Instructor/CreateInstructor/CreateInstructorCommandHandler.cs
+++ Services/Commands/Instructor/CreateInstructor/CreateInstructorCommandHandler.cs
@@ -1,19 +1,22 @@
-﻿using Infrastructure.Context;
+﻿using Domain.Interfaces;
+using Infrastructure.Context;
 
 namespace Services.Commands.Instructor.CreateInstructor;
 
 public class CreateInstructorCommandHandler
 {
     private readonly IttemContext _dbContext;
+    private readonly IAuthService _authService;
 
-    public CreateInstructorCommandHandler(IttemContext dbContext)
+    public CreateInstructorCommandHandler(IttemContext dbContext, IAuthService authService)
     {
         _dbContext = dbContext;
+        _authService = authService;
     }
 
     public async Task<dynamic> CreateInstructor(CreateInstructorCommand command)
     {
-        var parsedEntity = command.ToEntity(_dbContext);
+        var parsedEntity = command.ToEntity(_dbContext, _authService);
         await _dbContext.Instructors.AddAsync(parsedEntity);
 
         await _dbContext.SaveChangesAsync();
@@ -22,7 +25,8 @@ public class CreateInstructorCommandHandler
         {
             Operation = "Create",
             parsedEntity.Id,
-            Instructor = parsedEntity.Name
+            Instructor = $"{parsedEntity.FirstName} {parsedEntity.LastName}",
+            parsedEntity.Role
         };
     }
 }
